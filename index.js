6c1d2dbff88092e51d375b696e6ed1e3caee8b68
OussamaBOUIZ <oussamabouizgarne861@gmail.com>
2021-03-26 18:01:29 +0100

    Add animations to the page items

--- index.js
+++ index.js
@@ -42,10 +42,94 @@ smoothLinks.forEach(function (el) {
   });
 });
 
-console.log("welcome");
-
-const newFeautre = function () {
-  console.log("welcome to our web site");
+//REVEALING ITEMS 'ABOUT SECTION'
+const aboutSection = document.querySelector("section.section-about");
+const showAboutItems = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  const aboutSectionItems = aboutSection.querySelectorAll("div");
+  aboutSectionItems.forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.remove("small-Opaque");
+    }, i * 50);
+  });
+  observer.unobserve();
 };
+const aboutSectionObserver = new IntersectionObserver(showAboutItems, {
+  root: null,
+  threshold: 0.8,
+});
+aboutSectionObserver.observe(aboutSection);
 
-newFeautre();
+//REVEALING COMPANY SECTION ITEMS
+const companySection = document.querySelector("section#company");
+const companyStats = document.querySelectorAll("article.company__stats ul li");
+const companyExplore = document.querySelector("article.company__explore");
+const showCompanyStats = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  companyStats.forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.remove("down-Opaque");
+    }, i * 150);
+    setTimeout(() => {
+      companyExplore.classList.remove("small-Opaque");
+    }, 1300);
+  });
+  observer.unobserve();
+};
+const companySectionObserver = new IntersectionObserver(showCompanyStats, {
+  root: null,
+  threshold: 0.5,
+});
+companySectionObserver.observe(companySection);
+
+//REVEAL PROJECTS ITEMS
+const projectsSection = document.querySelector("#projects");
+const projectsItems = document.querySelectorAll("#projects > *");
+const showProjectsItems = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  projectsItems.forEach((el, i) => {
+    setTimeout(() => {
+      el.classList.remove("down-Opaque-contact");
+    }, i * 150);
+  });
+  observer.unobserve();
+};
+const projectsSectionObserver = new IntersectionObserver(showProjectsItems, {
+  root: null,
+  threshold: 0.8,
+});
+projectsSectionObserver.observe(projectsSection);
+
+//REVEAL CONTACT SECTION
+const contactSection = document.querySelector("#contact");
+const contactItems = document.querySelectorAll("#contact figure,form");
+const showContactSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  contactItems.forEach((el) => el.classList.remove("down-Opaque-contact"));
+  observer.unobserve();
+};
+const contactSectionObserver = new IntersectionObserver(showContactSection, {
+  root: null,
+  threshold: 0.5,
+});
+contactSectionObserver.observe(contactSection);
+
+//REVEAL FOOTER
+const footer = document.querySelector("footer");
+const footerItems = document.querySelectorAll("footer > *");
+const showFooterSection = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  footerItems.forEach((el) => el.classList.remove("small-Opaque"));
+  observer.unobserve();
+};
+const footerSectionObserver = new IntersectionObserver(showFooterSection, {
+  root: null,
+  threshold: 0.3,
+});
+footerSectionObserver.observe(footer);
